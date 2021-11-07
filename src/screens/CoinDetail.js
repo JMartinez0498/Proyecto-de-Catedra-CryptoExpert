4bc58218fd13f62f24b33a61b9c1e2d2b5b0d7c4
Marlin Ardon <marlinardon403@outlook.es>
2021-11-06 22:48:05 -0600

    Agregando pantalla estadisticas y correccion de pantalla detalle moneda

--- src/screens/CoinDetail.js
+++ src/screens/CoinDetail.js
@@ -14,116 +14,66 @@ import {
 import {colors} from '../util/colors';
 import HeaderBar from '../components/HeaderBar';
 import {ScrollView} from 'react-native-gesture-handler';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faArrowLeft} from '@fortawesome/free-solid-svg-icons';
 import {
   VictoryBar,
   VictoryLine,
   VictoryChart,
   VictoryTheme,
   VictoryAxis,
+  VictoryLabel
 } from 'victory-native';
 
 const CoinDetail = ({route, navigation}) => {
   const [selectedCurrency, setSelectedCurrency] = React.useState();
+  const [price,setPrice]=React.useState([]);  
   React.useEffect(() => {
     const {currency} = route.params;
     setSelectedCurrency(currency);
+    currency["item"]["sparkline_in_7d"]["price"].forEach(function (value,i){
+      let price_spark={
+        hour: i,
+        price:value
+      }           
+      setPrice(price=>[...price,price_spark]);
+    })
+    
     console.log(route.params);
-  }, []);
+  }, [selectedCurrency]);
 
   const imageUrl = JSON.stringify(selectedCurrency?.item.image);
   const string = imageUrl?.replace(/['"{}]/g, '');
 
   //** Arreglo de datos para dar formato x,y a los valores obtenidos desde la api para crear grafica de los ultimos 7 dias **//
 
-  const data = [
-    {day: 1, price: 61410},
-    {day: 2, price: 60578},
-    {day: 3, price: 61401},
-    {day: 4, price: 57321},
-    {day: 5, price: 57471},
-    {day: 6, price: 56268},
-    {day: 7, price: 56772},
-  ];
-
-
-//var json = JSON.stringify(selectedCurrency?.item.sparkline_in_7d)
-
-var time = []
-var day = 0
-
-for (var i = day; i < 168; i++) {
-    time.push(i*3600);
-}
-
-var arrayDay =  new Array(168).fill("day:")
-
-var arrayPrice =  new Array(168).fill("price:")
-
-
-var newArray = arrayDay.map((e, i) => e + time[i] + arrayPrice[i] + selectedCurrency?.item.sparkline_in_7d.price[i] );
-
-var prueba = JSON.stringify(data)
-
-const jsonString = JSON.stringify(Object.assign({}, newArray))
-
-
-
-
-
-
-const formatSparkline = (numbers) => {
-  const sevenDaysAgo = moment().subtract(7, 'days').unix();
-  let formattedSparkline = numbers.map((item, index) => {
-    return {
-      x: sevenDaysAgo + (index + 1) * 3600,
-      y: item,
-    }
-  })
-
-  return formattedSparkline;
-}
-
-const formatMarketData = (data) => {
-  let formattedData = [];
-
-  data.forEach(item => {
-    const formattedSparkline = formatSparkline(item.sparkline_in_7d.price)
-
-    const formattedItem = {
-      ...item,
-      sparkline_in_7d: {
-        price: formattedSparkline
-      }
-    }
-
-    formattedData.push(formattedItem);
-  });
-
-  return formattedData;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  function renderHeader() {
+    return (
+      <View style={styles.base}>
+        <View
+          style={{
+            flexDirection: 'row',
+            marginTop: 15,
+            alignItems: 'flex-start',
+          }}>
+          <View>
+            <View style={styles.base}>
+              <View style={styles.containerb}>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                  <Text style={styles.texth}>
+                    <FontAwesomeIcon
+                      icon={faArrowLeft}
+                      style={styles.icon}
+                      size={20}
+                    />{' '} {selectedCurrency?.item.name} ({selectedCurrency?.item.symbol})                         </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  }
 
   function renderChart() {
     return (
@@ -135,36 +85,33 @@ const formatMarketData = (data) => {
             alignItems: 'flex-start',
           }}>
           <View>
-          <View style={styles.base}>
-            <View style={styles.container}>
-                <TouchableOpacity onPress={() => navigation.goBack()}>
-                    <Text style={styles.text}><FontAwesomeIcon icon={ faArrowLeft } style={ styles.icon } size={20}/>  {selectedCurrency?.item.name}</Text>
-                </TouchableOpacity>
+            <View style={styles.base}>
+              <View style={styles.container}></View>
             </View>
-        </View>
-            <Image source={{uri: string}} style={styles.image} />
+  
           </View>
-          <Text style={styles.title}>{selectedCurrency?.item.name}</Text>
-          <Text style={styles.title}>({selectedCurrency?.item.symbol})</Text>
+          <Text style={styles.title2}>  <Image source={{uri: string}} style={styles.image} />    ${selectedCurrency?.item.current_price}      </Text>
+
         </View>
         <View style={styles.chart}>
           <VictoryChart
+          
             width={375}
-            domain={{x: [7, 1]}}
+            domain={{x: [167, 0]}}
             theme={VictoryTheme.material}>
             <VictoryLine
-              data={data}
+              data={price}
               style={{
                 data: {stroke: '#FFFF00'},
-                parent: {border: '1px solid #ccc'},
+                parent: {border: '1px solid #fff'},
               }}
               x="day"
               y="price"
             />
           </VictoryChart>
         </View>
-              <Text>{jsonString}</Text>
-              <Text>{prueba}</Text>
+        <Text>{}</Text>
+        <Text>{}</Text>
         <Text style={styles.text}>Otros datos</Text>
         <View style={{flexDirection: 'row'}}>
           <View style={styles.baseb}>
@@ -194,10 +141,10 @@ const formatMarketData = (data) => {
 
   return (
     <SafeAreaView style={styles.basearea}>
-      <HeaderBar />
       <ScrollView
         showsVerticalScrollIndicator={false}
         showsHorizontalScrollIndicator={false}>
+        <View style={styles.container}>{renderHeader()}</View>
         <View style={styles.card}>{renderChart()}</View>
       </ScrollView>
     </SafeAreaView>
@@ -210,8 +157,8 @@ const styles = StyleSheet.create({
   base: {
     flex: 1,
     backgroundColor: colors.background,
-    paddingHorizontal: 15,
-    paddingVertical: 10,
+    paddingHorizontal: 1,
+    paddingVertical: 5,
   },
   baseb: {
     flex: 1,
@@ -248,6 +195,11 @@ const styles = StyleSheet.create({
     fontSize: 25,
     marginLeft: 10,
   },
+  title2: {
+    color: colors.text,
+    fontSize: 30,
+    marginLeft: 10,
+  },
   button: {
     backgroundColor: colors.button,
   },
@@ -265,5 +217,30 @@ const styles = StyleSheet.create({
   image: {
     height: 35,
     width: 35,
+    marginLeft: -25,
+  },
+  texth: {
+    color: colors.text,
+    fontSize: 30,
+    marginLeft: 5,
+  },
+  icon: {
+    color: 'white',
+  },
+  touchable: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'flex-start',
+  },
+  containerb: {
+    flex: 1,
+    alignItems: 'flex-start',
+    marginLeft: 0,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    flexDirection: 'row',
   },
 });
--- src/screens/myCryptosScreens/Statistics.js
+++ src/screens/myCryptosScreens/Statistics.js
@@ -1,15 +1,58 @@
-import React, { useContext, useState } from 'react';
-import {View, Text, Button, Image,TextInput,StyleSheet,TouchableOpacity} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {
+  View,
+  Text,
+  Button,
+  Image,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+  ScrollView,
+} from 'react-native';
 import {colors} from '../../util/colors';
+import {
+  VictoryBar,
+  VictoryLine,
+  VictoryChart,
+  VictoryTheme,
+  VictoryAxis,
+} from 'victory-native';
 
 const Statitics = ({navigation}) => {
+  let priceChange = 10;
+
+  let inversion = 1500;
+  let carteta = 2500;
 
   return (
-    <View style={styles.base}>
-      <Text style={styles.text}>Estadisticas</Text>
-    </View>
+    <SafeAreaView style={styles.basearea}>
+      <ScrollView>
+        <View style={styles.basec}>
+          <Text style={styles.title2}> $ 14000</Text>
+        </View>
+        <View style={styles.basec}>
+
+        <View style={styles.baseb}>
+          <Text style={priceChange > 0 ? styles.priceChange : styles.priceChangeNegative}>-10%</Text>
+        </View></View>
+        <VictoryChart theme={VictoryTheme.material} domainPadding={{x: 130}}>
+          <VictoryBar
+          barRatio={55}
+            barWidth={({index}) => index * 2 + 70}
+            style={{
+              data: {fill: '#F4D35E'},
+            }}
+            data={[
+              {x: 'Inversion', y: inversion},
+              {x: 'Cartera', y: carteta},
+            ]}
+          />
+        </VictoryChart>
+      </ScrollView>
+    </SafeAreaView>
   );
-}
+};
 
 export default Statitics;
 
@@ -17,10 +60,110 @@ const styles = StyleSheet.create({
   base: {
     flex: 1,
     backgroundColor: colors.background,
+    paddingHorizontal: 1,
+    paddingVertical: 5,
+  },
+  baseb: {
+    flex: 1,
+    backgroundColor: colors.background,
     paddingHorizontal: 15,
     paddingVertical: 10,
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+  basec: {
+    flex: 1,
+    backgroundColor: colors.background,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+  },
+  basearea: {
+    flex: 1,
+    backgroundColor: colors.background,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    marginBottom: -130,
   },
   text: {
     color: colors.text,
+    fontSize: 15,
+    marginLeft: 5,
+    marginBottom: 10,
+    marginTop: 15,
+  },
+  texta: {
+    color: '#fff',
+    fontSize: 15,
+    marginLeft: 5,
+    marginBottom: 10,
+    marginTop: 15,
+  },
+  title: {
+    color: colors.text,
+    fontSize: 25,
+    marginLeft: 10,
+  },
+  title2: {
+    color: colors.text,
+    fontSize: 30,
+    marginLeft: 10,
+  },
+  button: {
+    backgroundColor: colors.button,
+  },
+  card: {
+    flex: 1,
+    paddingBottom: 130,
+  },
+  chart: {
+    flex: 1,
+    margin: 10,
+    alignItems: 'center',
+    backgroundColor: colors.background,
+    //paddingVertical: 180,
+  },
+  image: {
+    height: 35,
+    width: 35,
+    marginLeft: -25,
+  },
+  texth: {
+    color: colors.text,
+    fontSize: 30,
+    marginLeft: 5,
+  },
+  icon: {
+    color: 'white',
+  },
+  touchable: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'flex-start',
+  },
+  containerb: {
+    flex: 1,
+    alignItems: 'flex-start',
+    marginLeft: 0,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    flexDirection: 'row',
+  },
+  priceChange: {
+    color: 'rgb(102,184,93)',
+    width: '25%',
+    textAlign: 'left',
+    //textAlignVertical: 'left',
+  },
+
+  priceChangeNegative: {
+    color: 'red',
+    width: '25%',
+    textAlign: 'left',
+    //textAlignVertical: 'left',
   },
 });
